3096f33cdcc7bbef6383544786ca6d2dd360b8a9
Sayandipp <142604424+Sayandipp@users.noreply.github.com>
2024-09-08 22:03:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -124,7 +124,7 @@ if 'DYNO' in environ:
     APP_NAME = str(getenv('APP_NAME'))
 
 else:
-    ON_HEROKU = False
+    ON_HEROKU = True
 HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
     URL = "loose-maure-sayandip-38b4cf71.koyeb.app/".format(FQDN)
